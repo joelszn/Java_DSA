5def97bee367e85ec07f5abcac749cb6db505a5c
joelszn <duranjoel.a@gmail.com>
2019-10-15 02:05:18 -0400

    moved over to IntelliJ for Udemy course consistency

---
+++ Arrays/src/academy/learnprogramming/arrays/Main.java
@@ -0,0 +1,29 @@
+package academy.learnprogramming.arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] intArray = new int[7];
+
+        intArray[0] = 20;
+        intArray[1] = 35;
+        intArray[2] = -15;
+        intArray[3] = 7;
+        intArray[4] = 55;
+        intArray[5] = 1;
+        intArray[6] = -22;
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+
+        // example of retrieving 7 and acting like udk the index
+        for (int i = 0; i < intArray.length; i++) {
+            if(intArray[i] == 7){
+                System.out.println("index = " + i);
+            }
+
+        }
+
+    }
+}
